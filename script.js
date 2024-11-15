88733f12ed92bed661a76ce7425aa1da824efee9
ec449 <ec449@njit.edu>
2024-11-14 20:38:49 -0500

    lab8

---
+++ script.js
@@ -0,0 +1,91 @@
+//Q1
+let age=18;
+let Studentname="Erica";
+console.log(age);
+console.log(Studentname);
+
+//Q2
+let grade=true;
+console.log(grade);
+//boolean
+ let studentgrade;
+ console.log(studentgrade);
+ //undefined
+
+ //Q3
+ let RollNumber = 15;
+ if (RollNumber>10){
+    console.log("The Roll Number is greater than 10");
+ }
+ 
+ //Q4
+ let isMember=true;
+ if (isMember=true){
+    console.log("Member discount applied");
+ }
+
+ //Q5
+ let startValue=50;
+ if (startValue>0){
+    console.log("postive")
+ }
+else if (startValue<0){
+    console.log("negative");
+}
+else{
+    console.log("zero");
+}
+
+//Q6
+let a=5
+let b=10
+console.log(a === b);
+
+//Q7
+let LastName="Carangui";
+let FirstName="Erica";
+let Studentage=18;
+console.log("Hello, "+ FirstName + " " + LastName + ", you are " + Studentage + " years old.");
+
+//Q8
+let num=50;
+if(num<100 && num>0){
+    console.log("The number is within range.");
+}
+
+//Q9
+let hasCar=true;
+let hasLicense=true;
+let hasInsurance=true;
+if (hasCar&&hasLicense&&hasInsurance){
+    console.log("You can drive legally.")
+}
+else{
+    console.log ("Check your driving eligibility.")
+}
+
+//Q10
+let num1=21
+if(num1 % 2===0){
+    console.log("The number is even.");
+}
+else{
+    console.log("The number is odd.");
+}
+
+//Q11
+let score=85;
+if (score>=90){
+   
+    console.log("Grade A");
+}
+else if(number>=80){
+    
+    console.log("Grade B");
+
+} 
+
+else{
+   console.log ("Grade C")
+
+}
